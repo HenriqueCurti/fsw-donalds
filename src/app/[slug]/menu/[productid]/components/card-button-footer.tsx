1d098cd8c3b39656b6505d55368594fa82e93f87
Henrique <henrique.v.curti@gmail.com>
2025-02-27 16:47:33 -0300

    add react hook form

--- src/app/[slug]/menu/[productid]/components/card-button-footer.tsx
+++ src/app/[slug]/menu/[productid]/components/card-button-footer.tsx
@@ -9,8 +9,43 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  clientName: z.string().trim().min(1, {
+    message: "Por favor, informe seu no para o pedido",
+  }),
+  clientPhone: z.string().trim().min(1, {
+    message: "Por favor, informe um telefone de contato para o pedido",
+  }),
+});
 
 export const CardButtonFooter = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      clientName: "",
+      clientPhone: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log("On Submit called");
+
+    console.log(values);
+  }
+
   return (
     <div>
       <Drawer>
@@ -24,12 +59,30 @@ export const CardButtonFooter = () => {
             <DrawerTitle>Are you absolutely sure?</DrawerTitle>
             <DrawerDescription>This action cannot be undone.</DrawerDescription>
           </DrawerHeader>
-          <DrawerFooter>
-            <Button>Submit</Button>
-            <DrawerClose>
-              <Button variant="outline">Cancel</Button>
-            </DrawerClose>
-          </DrawerFooter>
+
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="clientName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nome</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Digite seu nome" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <DrawerFooter>
+                <Button type="submit">Finalizar Pedido</Button>
+                <DrawerClose asChild>
+                  <Button variant="outline">Cancelar</Button>
+                </DrawerClose>
+              </DrawerFooter>
+            </form>
+          </Form>
         </DrawerContent>
       </Drawer>
     </div>
